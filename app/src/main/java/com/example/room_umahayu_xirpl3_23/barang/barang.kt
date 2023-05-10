ce6ece1efe89f86383df728b2d73814aa92bb6c3
setyagita11 <umafagansa@gmail.com>
2023-05-09 23:48:20 -0700

    Menambahkan dependencies dan Membuat kotlinClass

---
+++ app/src/main/java/com/example/room_umahayu_xirpl3_23/barang/barang.kt
@@ -0,0 +1,8 @@
+package com.example.room_umahayu_xirpl3_23.barang
+
+data class barang (
+    val kode_brg: String,
+    val nama_brg: String,
+    val harga: Int,
+    val jumlah_brg: Int
+        )
---
+++ app/src/main/java/com/example/room_umahayu_xirpl3_23/buku/buku.kt
@@ -0,0 +1,7 @@
+package com.example.room_umahayu_xirpl3_23.buku
+
+data class buku (
+    val kode_buku: String,
+    val judul_buku: String,
+    val penulis: String
+        )
