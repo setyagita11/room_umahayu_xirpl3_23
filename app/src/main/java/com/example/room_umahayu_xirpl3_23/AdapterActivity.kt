29f73baf5e69657c48b3132f464482422901d87d
setyagita11 <Umafagansa@gmail.com>
2023-05-31 08:40:27 +0700

    Membuat db, DAO, MainAct, activity_main.xml

---
+++ app/src/main/java/com/example/room_umahayu_xirpl3_23/AdapterActivity.kt
@@ -0,0 +1,51 @@
+package com.example.room_umahayu_xirpl3_23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.room_umahayu_xirpl3_23.dataroom.User
+
+class AdapterActivity(private val list: ArrayList<User>, private val listener: onAdapterListener):
+    RecyclerView.Adapter<AdapterActivity.ViewHolder>(){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val nis = itemView.findViewById<TextView>(R.id.tv_nis)
+        val nama = itemView.findViewById<TextView>(R.id.tv_nama)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.activity_adapter, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = list[position]
+        holder.nis.text = user.idUser.toString()
+        holder.nama.text = user.namaUser.toString()
+        holder.nis.setOnClickListener{
+            listener.onClick(user)
+        }
+    }
+
+    override fun getItemCount() = list.size
+
+    fun setData (listData: List<User>){
+        list.clear()
+        list.addAll(listData)
+        notifyDataSetChanged()
+    }
+    interface onAdapterListener{
+        fun onClick(
+            user: User
+        )
+    }
+}
+
+
